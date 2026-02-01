529b34a53785278572b33a702fea95a3093ae0e9
Ayoola <47798700+casstimmy@users.noreply.github.com>
2026-02-01 10:34:37 +0100

    Setting up Treatment form and input

--- pages/manage/treatments.js
+++ pages/manage/treatments.js
@@ -211,8 +211,6 @@ export default function Treatments() {
               <tr>
                 <th className="px-2 py-2">Date</th>
                 <th className="px-2 py-2">Animal ID</th>
-                <th className="px-2 py-2">Breed</th>
-                <th className="px-2 py-2">Gender</th>
                 <th className="px-2 py-2">Routine</th>
                 <th className="px-2 py-2">Symptoms</th>
                 <th className="px-2 py-2">Possible Cause</th>
